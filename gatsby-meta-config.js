29e824eaeae5db9006b56f81b4cb532a71083f8e
KwonKusang <rnjsrntkd95@gmail.com>
2020-01-12 04:54:53 +0900

    initial KUSG.io

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `KUSG.io`,
+  description: `Kwon Ku Sang record and portfolio`,
+  author: `[KU_SANG]`,
+  introduction: `Back-End 개발자가 되기 위한 개발로그. @경기대학교 @P'rogramming-12th`,
+  siteUrl: `https://rnjsrntkd95.github.io`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `https://github.com/rnjsrntkd95`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
   },
--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/rnjsrntkd95/rnjsrntkd95.github.io"
       className="github"
       aria-label="GitHub"
     >
